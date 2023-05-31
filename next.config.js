60ce24eda9cb76bfeaba9b274f39301e00043874
Steve <ailurussteve@gmail.com>
2023-05-31 20:04:51 +0700

    edit next config

--- next.config.js
+++ next.config.js
@@ -1,38 +1,22 @@
 /** @type {import('next').NextConfig} */
-
-const nextConfig = {
-    reactStrictMode: true,
-    env: {
-        // defaults to TRUE, unless API Keys are set at build time; this flag is used by the UI
-        REQUIRE_USER_API_KEYS: !process.env.OPENAI_API_KEY,
-    },
+const webpack = require('webpack');
+module.exports = {
     experimental: {
-        runtime: 'edge',
+        runtime: 'experimental-edge',
     },
-    webpack(config, {buildId, dev, isServer, defaultLoaders, webpack}) {
-        // @mui/joy: anything material gets redirected to Joy
-        config.resolve.alias['@mui/material'] = '@mui/joy';
-
-        // @dqbd/tiktoken: enable asynchronous WebAssembly
-        config.experiments = {
-            asyncWebAssembly: true,
-            layers: true,
-        };
-
+    webpack: (config, {buildId, dev, isServer, defaultLoaders, webpack}) => {
         config.plugins.push(new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery',
             'window.jQuery': 'jquery'
         }))
-
         return config;
     },
+    reactStrictMode: true,
     eslint: {
         ignoreDuringBuilds: true,
     },
     typescript: {
         ignoreBuildErrors: true,
     }
-};
-
-module.exports = nextConfig
+}
