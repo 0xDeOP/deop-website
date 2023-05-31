c45b9e32c5bde492005d2c2d1971bfbdeab167ee
Steve <ailurussteve@gmail.com>
2023-05-31 19:48:46 +0700

    edit next config

--- next.config.js
+++ next.config.js
@@ -1,20 +1,57 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {experimental: {runtime: 'edge'}};
-const webpack = require('webpack');
-module.exports = {
-    webpack: (config, {buildId, dev, isServer, defaultLoaders, webpack}) => {
+// const nextConfig = {experimental: {runtime: 'edge'}};
+// const webpack = require('webpack');
+// module.exports = {
+//     webpack: (config, {buildId, dev, isServer, defaultLoaders, webpack}) => {
+//         config.plugins.push(new webpack.ProvidePlugin({
+//             $: 'jquery',
+//             jQuery: 'jquery',
+//             'window.jQuery': 'jquery'
+//         }))
+//         return config;
+//     },
+//     reactStrictMode: true,
+//     eslint: {
+//         ignoreDuringBuilds: true,
+//     },
+//     typescript: {
+//         ignoreBuildErrors: true,
+//     }
+// }
+
+const nextConfig = {
+    reactStrictMode: true,
+    env: {
+        // defaults to TRUE, unless API Keys are set at build time; this flag is used by the UI
+        REQUIRE_USER_API_KEYS: !process.env.OPENAI_API_KEY,
+    },
+    experimental: {
+        runtime: 'edge',
+    },
+    webpack(config, {buildId, dev, isServer, defaultLoaders, webpack}) {
+        // @mui/joy: anything material gets redirected to Joy
+        config.resolve.alias['@mui/material'] = '@mui/joy';
+
+        // @dqbd/tiktoken: enable asynchronous WebAssembly
+        config.experiments = {
+            asyncWebAssembly: true,
+            layers: true,
+        };
+
         config.plugins.push(new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery',
             'window.jQuery': 'jquery'
         }))
+
         return config;
     },
-    reactStrictMode: true,
     eslint: {
         ignoreDuringBuilds: true,
     },
     typescript: {
         ignoreBuildErrors: true,
     }
-}
+};
+
+module.exports = nextConfig;
